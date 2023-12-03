1997bd1204e8990a244958439dc648863023a88a
Mvycas <martynas.vycas@hotmail.com>
2023-12-03 15:43:55 +0100

    filtering fixed. Still need to fix Components

--- src/components/filterYearComponent.tsx
+++ src/components/filterYearComponent.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { filterByYear } from '../thunks/filterThunk';
-import { filterByDirector } from '../thunks/filterByDirectorThunk';
-import type { RootState, AppDispatch } from '../store';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { filterByYear } from "../thunks/filterThunk";
+import { filterByDirector } from "../thunks/filterThunk";
+import type { RootState, AppDispatch } from "../store";
+import { useNavigate } from "react-router-dom";
 
 const FilterComponent = () => {
   const [showYears, setShowYears] = useState(false);
   const [selectedYear, setSelectedYear] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [showSearchBar, setShowSearchBar] = useState(false);
 
   // Using useSelector to get data from the Redux store
   const movies = useSelector((state: RootState) => state.filterReducer.movies);
-  const directors = useSelector((state: RootState) => state.filterByDirector.movies);
+  const directors = useSelector(
+    (state: RootState) => state.filterReducer.movies
+  );
 
   console.log("Directors: ", directors);
-  
 
   const dispatch: AppDispatch = useDispatch();
   const navigate = useNavigate();
@@ -31,19 +32,22 @@ const FilterComponent = () => {
   };
 
   const handleYearSelection = (year) => {
-    console.log('Selected Year:', year);
+    console.log("Selected Year:", year);
     setSelectedYear(year);
     setShowSearchBar(false);
 
     // Dispatch the thunk based on the selected year
-    dispatch(filterByYear(year)).catch((error) => console.error('Error:', error));
+    dispatch(filterByYear(year)).catch((error) =>
+      console.error("Error:", error)
+    );
   };
 
   const handleSearch = () => {
     // Dispatch the thunk based on the search query
-    dispatch(filterByDirector(searchQuery)).catch((error) => console.error('Error:', error));
-    console.log("Directors after dispatching: " , directors);
-    
+    dispatch(filterByDirector(searchQuery)).catch((error) =>
+      console.error("Error:", error)
+    );
+    console.log("Directors after dispatching: ", directors);
   };
 
   const renderMovies = () => (
@@ -53,7 +57,6 @@ const FilterComponent = () => {
         movies.map((entry, index) => (
           <div key={index}>
             <p>{`Movie: ${entry.title} (${entry.year})`}</p>
-           
           </div>
         ))
       ) : (
@@ -61,13 +64,12 @@ const FilterComponent = () => {
       )}
     </div>
   );
-  
 
-  const years = Array.from({ length: new Date().getFullYear() - 1950 + 1 }, (_, index) => 1950 + index);
+  const years = Array.from(
+    { length: new Date().getFullYear() - 1950 + 1 },
+    (_, index) => 1950 + index
+  );
   let temp = 0;
-
-  
-  
 
   return (
     <div>
@@ -89,7 +91,7 @@ const FilterComponent = () => {
             {years.map((year) => (
               <Button
                 key={year}
-                variant={selectedYear === year ? 'info' : 'light'}
+                variant={selectedYear === year ? "info" : "light"}
                 onClick={() => handleYearSelection(year)}
                 className="mr-2 mb-2"
               >
@@ -117,18 +119,16 @@ const FilterComponent = () => {
           )}
         </Form.Group>
         {directors && directors.length > 0 && (
-        <div className="mt-2">
-          <h5>Movies by director:</h5>
-          
-          <ul>
-          {directors.map((item, index) => (
-           <li key={index}>{item.movie_id?.title}</li>
+          <div className="mt-2">
+            <h5>Movies by director:</h5>
 
-          ))}
-            
-          </ul>
-        </div>
-      )}
+            <ul>
+              {directors.map((item, index) => (
+                <li key={index}>{item.movie_id?.title}</li>
+              ))}
+            </ul>
+          </div>
+        )}
       </Form>
     </div>
   );
