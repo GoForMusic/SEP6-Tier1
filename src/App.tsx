32baaa030bbae3c570ab0d11f10a8384d23ee3be
Mvycas <martynas.vycas@hotmail.com>
2023-12-06 01:24:30 +0100

    merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import RegisterUserScreen from "./screens/registerUserScreen";
 import FilterYearComponent from "./components/filterYearComponent";
 import LoginScreen from "./screens/loginUserScreen";
 import FilterByRateComponent from "./components/filterByRateComponent";
-import Home from "./components/home/Home";
+import HomeScreen from '../src/screens/homeScreen'
 
 const App = () => {
   return (
@@ -16,12 +16,11 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/helloworld" element={<HelloWorldScreen />} />
-            <Route path="/movies" element={<Home />} />
             <Route path="/account" element={<RegisterUserScreen />} />
-            <Route path="/filter" element={<FilterYearComponent />} />
-            <Route path="/login" element={<LoginScreen />} />
-            <Route path="/rate" element={<FilterByRateComponent />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/filter" element={<FilterYearComponent/>} />
+            <Route path="/account/login" element={<LoginScreen/>}/>
+            <Route path="/rate" element={<FilterByRateComponent/>}/>
+            <Route path="/home" element={<HomeScreen/>}/>
           </Routes>
         </Container>
       </main>
