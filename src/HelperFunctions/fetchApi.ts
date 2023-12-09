63c22968b92a3f1b59b4ae01ef544bc2c9af58ee
VassilevEmil <emilvassilev5@abv.bg>
2023-12-09 23:57:22 +0100

    saving the process

--- src/HelperFunctions/fetchApi.ts
+++ src/HelperFunctions/fetchApi.ts
@@ -1,4 +1,3 @@
-
 //##########################################################################################
 //ABSTRACT LOGIC FOR CODE REUSABILITY: (this piece is always used with each request,
 //        since we have many same kind of requests, there was a need to create such function
@@ -11,25 +10,24 @@ import fetchJsonp from "./fetchJsonp";
 
 const API_BASE_URL = "https://tier2.azurewebsites.net";
 const TMDB_BASE_URL = "https://api.themoviedb.org";
-const TMDB_apiKey = process.env.REACT_APP_TMDB_API_KEY;
-
+const TMDB_apiKey = "7ab06a3836f7b4eac34256dcaa05a6c7";
 
 export async function fetchFromAzure(endpoint: string, method: string = "GET") {
-    const response = await fetch(`${API_BASE_URL}${endpoint}`, {
-      mode: "cors",
-      method: method,
-      headers: { "Content-Type": "application/json" },
-    });
-    return response.json();
-  }
-  
+  const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+    mode: "cors",
+    method: method,
+    headers: { "Content-Type": "application/json" },
+  });
+  return response.json();
+}
+
 export async function fetchFromTMDB_api(movieId: string): Promise<MovieData> {
-    const url = `${TMDB_BASE_URL}/3/movie/${movieId}?api_key=${TMDB_apiKey}`;
-    try {
-      const response = await fetchJsonp(url, "callbackFunctionName");
-      return response as MovieData; // Type assertion to MovieData
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      throw error;
-    }
+  const url = `${TMDB_BASE_URL}/3/movie/${movieId}?api_key=${TMDB_apiKey}`;
+  try {
+    const response = await fetchJsonp(url, "callbackFunctionName");
+    return response as MovieData; // Type assertion to MovieData
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
   }
+}
