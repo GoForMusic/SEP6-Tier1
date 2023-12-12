97ef1994fe13bb2252ef976677811c063f0b6947
Mvycas <martynas.vycas@hotmail.com>
2023-12-12 22:10:15 +0100

    fixed movie details refresh

--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -3,42 +3,38 @@ import { Link, useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { fetchFromTMDB_api } from "../../HelperFunctions/fetchApi";
 import type { RootState, AppDispatch } from "../../store";
-import {
-    ImgStyled,
-  } from "../../components/home/Styling/home_style";
-  import CommentComponent from '../comments/commentComponent'
-  import { FacebookShareButton, WhatsappShareButton } from "react-share";
+import { ImgStyled } from "../../components/home/Styling/home_style";
+import CommentComponent from "../comments/commentComponent";
+import { FacebookShareButton, WhatsappShareButton } from "react-share";
+import "./details.css";
 
-
-import "./details.css"; 
-
-const MovieDetails = () => {
+const Details = () => {
   const { movieId } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
   const [loading, setLoading] = useState(true);
   const [copied, setCopied] = useState(false);
 
-  const movies = useSelector((state: RootState) => state.movieReducer.movies);
-
   useEffect(() => {
+    console.log("e", movieId);
     const fetchMovieDetails = async () => {
       let goodString = movieId.toString().padStart(7, "0");
       try {
         const movieData = await fetchFromTMDB_api(`tt${goodString}`);
         setMovieDetails({
           title: movieData.Title,
+          poster: movieData.poster_path,
           genres: movieData.genres.map((genre) => genre.name),
           overview: movieData.overview,
           year: movieData.Year,
-          language: movieData.original_language, 
-          vote: movieData.vote_average
+          language: movieData.original_language,
+          vote: movieData.vote_average,
           // Add other movie details as needed
         });
       } catch (error) {
         console.error(`Error fetching details for movie ID ${movieId}:`, error);
         setMovieDetails(null); // Handle error state if needed
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     };
 
@@ -47,12 +43,6 @@ const MovieDetails = () => {
 
   if (!movieDetails) {
     return <p>Loading...</p>; // or handle loading/error state in a different way
-  }
-
-  const currentMovie = movies.find((movie) => movie.id === Number(movieId));
-
-  if (!currentMovie) {
-    return <p>Movie not found</p>;
   }
 
   function copy() {
@@ -86,16 +76,18 @@ const MovieDetails = () => {
         <strong>IDMB Vote:</strong> {movieDetails.vote}
       </p>
       <div>
-        <ImgStyled src={currentMovie.poster} alt={currentMovie.title} />
+        <ImgStyled
+          src={`https://image.tmdb.org/t/p/original/${movieDetails.poster}`}
+          alt={movieDetails.title}
+        />
       </div>
-    
       <CommentComponent movieId={movieId} />
       <button onClick={copy}>{!copied ? "Copy link" : "Copied!"}</button>
       <div>
         <FacebookShareButton url={shareUrl} title={movieDetails.title}>
           Share on Facebook
         </FacebookShareButton>
-        <br/>
+        <br />
         <WhatsappShareButton url={shareUrl} title={movieDetails.title}>
           Share on Whatsapp
         </WhatsappShareButton>
@@ -105,5 +97,4 @@ const MovieDetails = () => {
   );
 };
 
-  
-export default MovieDetails;
+export default Details;
